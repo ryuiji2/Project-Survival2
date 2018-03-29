73fba5fe37d44f6c6aef51de98ef9bc69f93bf63
Avonexile <arnevanommen@outlook.com>
2018-03-29 11:09:16 +0200

    testing

---
+++ Unity/Project Survival/Assets/Classes/Arne/Scripts/TestPlayerStats.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerStats : MonoBehaviour {
+
+	//weapons
+	public List<GameObject> weapons = new List<GameObject>();
+	public GameObject currentWeapon;
+
+	//health 
+	public float maxHP;
+	public float health;
+	public float healthPercentage;
+	UIManager uim;
+
+	public Vector3 startPos;
+
+
+
+	private void Awake () {
+
+		uim = GameObject.Find("Canvas").GetComponent<UIManager>(); //gives error needs to be manually put in
+		maxHP = 100;
+		health = maxHP;
+		startPos = transform.position;
+	}
+	public void PlayerHealth (float dmg) {
+
+		health -= dmg;
+		healthPercentage = health / maxHP;
+		if(health <= 0f) {
+
+			Debug.Log("Dead");	
+			uim.SetState(UIManager.UIState.GameOver);
+		}
+		uim.CheckHealth();
+	}
+	public void PlayerReset () {
+
+		transform.position = startPos;
+		health = maxHP;
+		PlayerHealth(0f);
+	}
+}
