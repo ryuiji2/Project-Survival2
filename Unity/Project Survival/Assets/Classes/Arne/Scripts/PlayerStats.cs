ceb2769e57d34139c1ce659f9073805a43136a40
Avonexile <arnevanommen@outlook.com>
2018-03-29 09:19:14 +0200

    stuff

--- Unity/Project Survival/Assets/Classes/Arne/Scripts/PlayerStats.cs
+++ Unity/Project Survival/Assets/Classes/Arne/Scripts/PlayerStats.cs
@@ -31,6 +31,7 @@ public class PlayerStats : MonoBehaviour {
 		healthPercentage = health / maxHP;
 		if(health <= 0f) {
 
+			Debug.Log("Dead");	
 			uim.SetState(UIManager.UIState.GameOver);
 		}
 		uim.CheckHealth();
